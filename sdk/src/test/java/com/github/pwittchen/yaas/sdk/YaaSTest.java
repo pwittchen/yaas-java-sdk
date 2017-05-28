b520e8cce6f70e7f0567089198e9bed5779bb94e
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-05-28 12:07:40 +0200

    adding first tests for YaaS class

--- sdk/src/test/java/com/github/pwittchen/yaas/sdk/YaaSTest.java
+++ sdk/src/test/java/com/github/pwittchen/yaas/sdk/YaaSTest.java
@@ -1,5 +1,58 @@
 package com.github.pwittchen.yaas.sdk;
 
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+
 public class YaaSTest {
-  //TODO: implement tests
+
+  @Test
+  public void shouldCreateYaaSObject() {
+    // given
+    YaaSProject project = mock(YaaSProject.class);
+
+    // when
+    Client client = new YaaS(project);
+
+    // then
+    assertThat(client).isNotNull();
+  }
+
+  @Test
+  public void shouldCreateYaaSObjectWithAuthorization() {
+    // given
+    YaaSProject project = mock(YaaSProject.class);
+    Authorization authorization = mock(Authorization.class);
+
+    // when
+    Client client = new YaaS(project, authorization);
+
+    // then
+    assertThat(client).isNotNull();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldNotCreateYaaSObjectWhenProjectIsNull() {
+    // given
+    YaaSProject project = null;
+    Authorization authorization = mock(Authorization.class);
+
+    // when
+    Client client = new YaaS(project, authorization);
+
+    // then exception is thrown
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldNotCreateYaaSObjectWhenAuthorizationIsNull() {
+    // given
+    YaaSProject project = mock(YaaSProject.class);
+    Authorization authorization = null;
+
+    // when
+    Client client = new YaaS(project, authorization);
+
+    // then exception is thrown
+  }
 }
